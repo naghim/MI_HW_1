257b13871426e69a9d0af3a446d63eecf8d9a52a
Naghi Mirtill - Boglárka <nagy.mirtill@gmail.com>
2020-03-01 19:18:22 +0200

    Added branching based on the argument
    Added branching based on the argument: 
    - 1: backtracking
    - 2: MVR + forward check
    - 3: MVR + AC-3

--- main.cpp
+++ main.cpp
@@ -13,32 +13,33 @@
 
 using namespace std;
 
-MINI B=1;   									///Buildings
-MINI M=2;   									///Room's repetitive properties - Mod
-MINI N_RT=2;									///Room types
-VAR N_D=0;  									///Number of rooms
-VAR N_X=0;  									///Number of residents
+MINI B = 1;   									///Buildings
+MINI M = 2;   									///Room's repetitive properties - Mod
+MINI N_RT = 2;									///Room types
+VAR N_D = 0;  									///Number of rooms
+VAR N_X = 0;  									///Number of residents
+MINI MODE = 1;									/// Solution mode
 
-map<VAR,set<VAR>> Dx;   						///Room pool - domain / people<personID, room_pool<roomID>>
+map<VAR, set<VAR>> Dx;   						///Room pool - domain / people<personID, room_pool<roomID>>
 vector<VAR> X;      							///Residents
 vector<MINI> RT;    							///Rooms with type
 
 vector< vector<bool> > C_M; 					///Room's repetitive properties constraint
 bool M_def = true;      						///Default value for M needs
-vector<pair<MINI,MINI>> RP = { {4,3}, {3,6} };  ///Room's Properties - Count/Capacity
+vector<pair<MINI, MINI>> RP = { {4,3}, {3,6} };  ///Room's Properties - Count/Capacity
 vector< vector<bool> > C_RT;					///Room type constraint
 bool RT_def = true;     						///Default value for room type
 vector< set<VAR> > C_F;  						///Requested roommates / friend constraint
 vector< set<VAR> > C_E;  						///Rejected roommates / enemy constraint
 
-struct State{
-    State();
-    State(VAR x, map<VAR,set<VAR>> d) : X_actual(x), D_actual(d){};
-    VAR X_actual;
-    map<VAR,set<VAR>> D_actual;
+struct State {
+	State();
+	State(VAR x, map<VAR, set<VAR>> d) : X_actual(x), D_actual(d) {};
+	VAR X_actual;
+	map<VAR, set<VAR>> D_actual;
 };
 
-stack<State> SBuilder;							
+stack<State> SBuilder;
 unsigned long long StepCounter = 0;
 
 void ReadData();
@@ -46,190 +47,207 @@ bool Solve();
 void WriteSolution();
 void WriteFailure();
 
-int main(){
-    ReadData();
-    if( Solve() ) WriteSolution();
-    else WriteFailure();
-    cout << "Az algoritmus lefutott " << StepCounter << " lepessel.\n";
-    return 0;
+int main(int argc, char* argv[]) {
+	int mode = (int)argv[1] - 48;
+	if (mode == 0 || mode == 1 || mode == 2) MODE == mode;
+	cout << "Megoldasi modszer: " << MODE << endl;
+
+	ReadData();
+	if (Solve()) WriteSolution();
+	else WriteFailure();
+	cout << "Az algoritmus lefutott " << StepCounter << " lepessel.\n";
+	return 0;
 }
 
-void ReadData(){
-    MINI tmp1, tmp2;
-    VAR tmp3, tmp4;
-    char tmpChar;
-    bool pre = true;
-    ifstream data("input.txt");
-    while(pre and data>>tmpChar) switch(tmpChar){
-    case 'C': pre=false; break;
-    case 'B': data>>B; break;
-    case 'M': data>>M; break;
-    case 'R':
-        data>>N_RT;
-        RP.clear();
-        for(MINI i=0; i<N_RT; i++){
-            data >> tmp1 >> tmp2; 		///How many rooms? Capacity?
-            RP.push_back( make_pair(tmp1,tmp2) );
-        } break;
-    }
-    for( auto& room : RP){ N_D += room.first; N_X += room.first*room.second; }
-    N_D *= B*M; N_X *= B*M;
-    cout << "Emberek szama: " << N_X << " \nSzobak szama: " << N_D << endl << endl;
+void ReadData() {
+	MINI tmp1, tmp2;
+	VAR tmp3, tmp4;
+	char tmpChar;
+	bool pre = true;
+	ifstream data("input.txt");
+	while (pre and data >> tmpChar) switch (tmpChar) {
+	case 'C': pre = false; break;
+	case 'B': data >> B; break;
+	case 'M': data >> M; break;
+	case 'R':
+		data >> N_RT;
+		RP.clear();
+		for (MINI i = 0; i < N_RT; i++) {
+			data >> tmp1 >> tmp2; 		///How many rooms? Capacity?
+			RP.push_back(make_pair(tmp1, tmp2));
+		} break;
+	}
+	for (auto& room : RP) { N_D += room.first; N_X += room.first * room.second; }
+	N_D *= B * M; N_X *= B * M;
+	cout << "Emberek szama: " << N_X << " \nSzobak szama: " << N_D << endl << endl;
 
-    C_M.clear();    C_M.resize( N_X, vector<bool>(M,M_def) );
-    C_RT.clear();   C_RT.resize( N_X, vector<bool>(N_RT,RT_def) );
-    C_F.clear();    C_F.resize( N_X, set<VAR>() );
-    C_E.clear();    C_E.resize( N_X, set<VAR>() );
-    while(data>>tmpChar) switch(tmpChar){
-        case 'M':
-            data>>tmp3;
-            for(VAR i=0; i<tmp3; i++){
-                data >> tmp4 >> tmp1;    /// Which room is okay or not okay for a person
-                C_M[tmp4][tmp1] = !M_def;
-            } break;
-        case 'T':
-            data>>tmp3;
-            for(VAR i=0; i<tmp3; i++){
-                data >> tmp4 >> tmp1;   /// Which room can a person occupy - has the right to occupy
-                C_RT[tmp4][tmp1]= !RT_def;
-            } break;
-        case 'F':
-            data>>tmp1;
-            for(MINI i=0; i<tmp1; i++){
-                data >> tmp3 >> tmp4;
-                C_F[tmp3].insert(tmp4); /// X wants to be with Y
-                C_F[tmp4].insert(tmp3); /// Let this be a mutual request 
-            } break;
-        case 'E':
-            data>>tmp1;
-            for(MINI i=0; i<tmp1; i++){
-                data >> tmp3 >> tmp4;
-                C_E[tmp3].insert(tmp4); /// X doesn't want to be with Y
-                C_E[tmp4].insert(tmp3); /// Let this also be a mutual request 
-            } break;
-    }
-    data.close();
+	C_M.clear();    C_M.resize(N_X, vector<bool>(M, M_def));
+	C_RT.clear();   C_RT.resize(N_X, vector<bool>(N_RT, RT_def));
+	C_F.clear();    C_F.resize(N_X, set<VAR>());
+	C_E.clear();    C_E.resize(N_X, set<VAR>());
+	while (data >> tmpChar) switch (tmpChar) {
+	case 'M':
+		data >> tmp3;
+		for (VAR i = 0; i < tmp3; i++) {
+			data >> tmp4 >> tmp1;    /// Which room is okay or not okay for a person
+			C_M[tmp4][tmp1] = !M_def;
+		} break;
+	case 'T':
+		data >> tmp3;
+		for (VAR i = 0; i < tmp3; i++) {
+			data >> tmp4 >> tmp1;   /// Which room can a person occupy - has the right to occupy
+			C_RT[tmp4][tmp1] = !RT_def;
+		} break;
+	case 'F':
+		data >> tmp1;
+		for (MINI i = 0; i < tmp1; i++) {
+			data >> tmp3 >> tmp4;
+			C_F[tmp3].insert(tmp4); /// X wants to be with Y
+			C_F[tmp4].insert(tmp3); /// Let this be a mutual request 
+		} break;
+	case 'E':
+		data >> tmp1;
+		for (MINI i = 0; i < tmp1; i++) {
+			data >> tmp3 >> tmp4;
+			C_E[tmp3].insert(tmp4); /// X doesn't want to be with Y
+			C_E[tmp4].insert(tmp3); /// Let this also be a mutual request 
+		} break;
+	}
+	data.close();
 }
 
-bool CanOccupy(VAR homie, VAR room){
-    if( C_M[homie][ room%M ] )  		/// Is the facing of the room okay?
-        if( C_RT[homie][ RT[room] ] )   /// Has the person the right to occupy it?
-            return true;
-    return false;
+bool CanOccupy(VAR homie, VAR room) {
+	if (C_M[homie][room % M])  		/// Is the facing of the room okay?
+		if (C_RT[homie][RT[room]])   /// Has the person the right to occupy it?
+			return true;
+	return false;
 }
-void Init(){
-    MINI roomType=0;
 
-    RT.clear();
-    for(const auto& room : RP) RT.resize( RT.size()+B*M*room.first , roomType++);
+void Init() {
+	MINI roomType = 0;
 
-    X.clear(); X.resize( N_X, HOMELESS );
-    for(VAR h=0; h<N_X; h++) 			/// h - homelesses
-        for(VAR r=0; r<N_D;  r++)
-            if( CanOccupy(h,r) ) Dx[h].insert(r);
+	RT.clear();
+	for (const auto& room : RP) RT.resize(RT.size() + B * M * room.first, roomType++);
+
+	X.clear(); X.resize(N_X, HOMELESS);
+	for (VAR h = 0; h < N_X; h++) 			/// h - homelesses
+		for (VAR r = 0; r < N_D; r++)
+			if (CanOccupy(h, r)) Dx[h].insert(r);
 
 }
-VAR MVR(const map<VAR,set<VAR>>&);
+
+VAR MVR(const map<VAR, set<VAR>>&);
 VAR OrderDomainValues(VAR, set<VAR>&);	///For whom and from which domain?
 bool incompatibleConstraint(VAR);
 State StepBuilder(const State&);
-bool Solve(){
-    Init();
+bool Solve() {
+	Init();
 
-    SBuilder.push(State( MVR(Dx), Dx ));
+	SBuilder.push(State(MVR(Dx), Dx));
 
-    while( !SBuilder.empty() ){
-        VAR homie = SBuilder.top().X_actual;
+	while (!SBuilder.empty()) {
+		VAR homie = SBuilder.top().X_actual;
 
-        ///We try one value
-        X[homie] = OrderDomainValues(homie, SBuilder.top().D_actual[homie] );
-        if( X[homie]==HOMELESS ){ 		///There's no more value
-                SBuilder.pop(); continue;
-        }else{ 							///There is a value
-            StepCounter++;
-            ///Are there incompatible constraints? If yes, then it will go to the next value with a simple continue;
-            if( incompatibleConstraint(homie) ) continue;
-        }
+		///We try one value
+		X[homie] = OrderDomainValues(homie, SBuilder.top().D_actual[homie]);
+		if (X[homie] == HOMELESS) { 		///There's no more value
+			SBuilder.pop(); continue;
+		}
+		else { 							///There is a value
+			StepCounter++;
+			///Are there incompatible constraints? If yes, then it will go to the next value with a simple continue;
+			if (incompatibleConstraint(homie)) continue;
+		}
 
-        if( SBuilder.size()==X.size() ) return true;
+		if (SBuilder.size() == X.size()) return true;
 
-        SBuilder.push( StepBuilder( SBuilder.top() ) );
-    }
-    ///There wasn't any solution and the stack is empty...
-    return false;
+		SBuilder.push(StepBuilder(SBuilder.top()));
+	}
+	///There wasn't any solution and the stack is empty...
+	return false;
 }
 
-VAR OrderDomainValues(VAR homie, set<VAR>& domain){
-    ///valamilyen sorrend (most az elso lehetseges)
-    EZ_MI_EZ?
-    VAR value = HOMELESS;
-    if( domain.size() >0  ){
-        value = *domain.begin();
-        domain.erase(domain.begin());	///This function shrinks the size of the domain
-    }
-    return value;
-}
-bool incompatibleConstraint(VAR x){		///For example: is there any room left for a new person?
-    MINI roomMates=0;
-    for(VAR Xvalue : X) if(Xvalue==X[x]) roomMates++; ///How many of them are in the room?
-    if( RP[RT[X[x]]].second < roomMates ) return true;
-
-    return false;
+VAR OrderDomainValues(VAR homie, set<VAR>& domain) {
+	VAR value = HOMELESS;
+	if (domain.size() > 0) {
+		value = *domain.begin();
+		domain.erase(domain.begin());	///This function shrinks the size of the domain
+	}
+	return value;
 }
 
-map<VAR,set<VAR>> DomainBuilder(const State&);
-State StepBuilder(const State& oldState){
-    map<VAR,set<VAR>> newDomain = DomainBuilder(oldState);
-    return State( MVR(newDomain), newDomain );
-}
-VAR MVR(const map<VAR,set<VAR>>& D_actual){
-    VAR newHomie, minDsize = N_D+1;		/// N_D became the D_max 
-    for(const auto& mit : D_actual) if( X[mit.first]==HOMELESS )
-        if( mit.second.size() < minDsize ){
-            minDsize = mit.second.size();
-            newHomie = mit.first;
-        }
+bool incompatibleConstraint(VAR x) {		///For example: is there any room left for a new person?
+	MINI roomMates = 0;
+	for (VAR Xvalue : X) if (Xvalue == X[x]) roomMates++; ///How many of them are in the room?
+	if (RP[RT[X[x]]].second < roomMates) return true;
 
-    return newHomie;
-}
-void AC3(VAR x, map<VAR,set<VAR>>& D_actual){
-    queue<VAR> friends;
-    for(const auto& xFriend :C_F[x]) friends.push(xFriend);
-    while( !friends.empty() ){
-        if( D_actual[ friends.front() ].erase( X[x] ) >0 ){
-            if( D_actual[ friends.front() ].size() >0 ){
-                D_actual[ friends.front() ].clear();
-                for(const auto& yFriend :C_F[friends.front()]) friends.push( yFriend );
-            }
-            D_actual[friends.front()].insert( X[x] );
-        }
-        friends.pop();
-    }
-}
-map<VAR,set<VAR>> DomainBuilder(const State& oldState){
-    map<VAR,set<VAR>> D_actual = oldState.D_actual;
-
-    VAR homie = oldState.X_actual;
-    for(const auto& enemies : C_E[homie]) D_actual[enemies].erase(X[homie]); ///Forward check
-
-    AC3( homie, D_actual );
-
-    D_actual[homie].clear();			///Optimisation
-    return D_actual;
+	return false;
 }
 
-void WriteSolution(){
-    map<VAR,set<VAR>> Assignment;
-    for(VAR x=0; x<X.size(); x++) Assignment[X[x]].insert(x);
-
-    cout << "A szobabeosztas:\n";
-    for(const auto& mit : Assignment){
-        cout << mit.first << ". szoba  ( ";
-        for(const auto& sit : mit.second)
-            cout << sit << " ";
-        cout << ")\n";
-    }
+map<VAR, set<VAR>> DomainBuilder(const State&);
+State StepBuilder(const State& oldState) {
+	map<VAR, set<VAR>> newDomain = DomainBuilder(oldState);
+	return State(MVR(newDomain), newDomain);
 }
-void WriteFailure(){
-    cout << "Nincs lehetseges szobabeosztas.\n";
+
+VAR MVR(const map<VAR, set<VAR>>& D_actual) {
+	VAR newHomie, minDsize = N_D + 1;		/// N_D became the D_max 
+	if (MODE > 1)
+	{
+		for (const auto& mit : D_actual) if (X[mit.first] == HOMELESS)
+			if (mit.second.size() < minDsize) {
+				minDsize = mit.second.size();
+				newHomie = mit.first;
+			}
+		return newHomie;
+	}
+	
+	for (const auto& mit : D_actual) return mit.first;
+	
+}
+
+void AC3(VAR x, map<VAR, set<VAR>>& D_actual) {
+	queue<VAR> friends;
+	for (const auto& xFriend : C_F[x]) friends.push(xFriend);
+	while (!friends.empty()) {
+		if (D_actual[friends.front()].erase(X[x]) > 0) {
+			if (D_actual[friends.front()].size() > 0) {
+				D_actual[friends.front()].clear();
+				for (const auto& yFriend : C_F[friends.front()]) friends.push(yFriend);
+			}
+			D_actual[friends.front()].insert(X[x]);
+		}
+		friends.pop();
+	}
+}
+
+map<VAR, set<VAR>> DomainBuilder(const State& oldState) {
+	map<VAR, set<VAR>> D_actual = oldState.D_actual;
+	VAR homie = oldState.X_actual;
+	
+	if (MODE > 1)
+	{
+		for (const auto& enemies : C_E[homie]) D_actual[enemies].erase(X[homie]); ///Forward check
+	}
+	if (MODE == 3) AC3(homie, D_actual);										  ///AC-3
+
+	D_actual[homie].clear();													  ///Memory optimisation
+	return D_actual;
+}
+
+void WriteSolution() {
+	map<VAR, set<VAR>> Assignment;
+	for (VAR x = 0; x < X.size(); x++) Assignment[X[x]].insert(x);
+
+	cout << "A szobabeosztas:\n";
+	for (const auto& mit : Assignment) {
+		cout << mit.first << ". szoba  ( ";
+		for (const auto& sit : mit.second)
+			cout << sit << " ";
+		cout << ")\n";
+	}
+}
+
+void WriteFailure() {
+	cout << "Nincs lehetseges szobabeosztas.\n";
 }
